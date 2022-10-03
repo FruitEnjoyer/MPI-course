2b44147f615ca1744dda41d4d3e5ba00c7c2f7f3
FruitEnjoyer <78922271+FruitEnjoyer@users.noreply.github.com>
2022-10-04 01:30:43 +0300

    Add files via upload

--- mpi_1.c
+++ mpi_1.c
@@ -3,12 +3,17 @@
 
 int main(int argc, char **argv)
 {
+	int process_size, process_id;
+	printf("Before MPI_INIT\n");
+	
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &process_size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &process_id);
+
+	printf("process: %i   size: %i\n", process_id, process_size);
+
+	MPI_Finalize();
+	printf("After MPI_FINALIZE\n");
   
-  printf("Before MPI_INIT\n");
-  MPI_Init(&argc, &argv);
-  printf("Parallel sect\n");
-  MPI_Finalize();
-  printf("After MPI_FINALIZE\n");
-  
-  return 0;
+	return 0;
 }
